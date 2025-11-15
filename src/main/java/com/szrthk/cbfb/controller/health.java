a7e1677d56f9eb05afea314429a5df6e6c159e55
szrthk <your-email@example.com>
2025-11-15 23:15:21 +0530

    changed string to map

--- src/main/java/com/szrthk/cbfb/controller/health.java
+++ src/main/java/com/szrthk/cbfb/controller/health.java
@@ -1,12 +1,14 @@
 package com.szrthk.cbfb.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class health {
     @GetMapping("/hc")
-    public String healthcheck(){
-        return "System is working fine";
+    public Map<String, String> healthcheck(){
+        return Map.of("status", "System is working fine");
     }
 }
